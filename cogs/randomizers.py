ac31dc45d6e4f44f29d70718079d712a9a4332bf
Zio Vela <86828677+duckgoose0@users.noreply.github.com>
2021-07-03 22:02:36 +0000

    added missing arg error for ?ask

--- cogs/randomizers.py
+++ cogs/randomizers.py
@@ -6,33 +6,34 @@ from discord.ext import commands
 
 class Randomizer(commands.Cog):
 
-    def __init__(self, commands):
-        self.commands = commands
+	def __init__(self, commands):
+		self.commands = commands
 
-    @commands.command()
-    async def ask(self, ctx, *, question):
-        responses = ['Yes :grin:',
-                     'Of course :grin:',
+	@commands.command()
+	async def ask(self, ctx, *, question : str):
+		responses = ['Yes :grin:',
+ 					 'Of course :grin:',
                      'Maybe :thinking:',
                      'I don\'t know :thinking:',
                      'No :pensive:',
                      'I don\'t think so :pensive:']
-        await ctx.send(f'''> {question}
+		await ctx.send(f'''> {question}
 {random.choice(responses)}''')
 
-    @commands.command(aliases=['flip', 'cointoss'])
-    async def coin(self, ctx):
-        responses = ['heads', 'tails']
-        await ctx.send(f'{random.choice(responses)}')
+	@commands.command(aliases=['flip', 'cointoss'])
+	async def coin(self, ctx):
+		responses = ['heads', 'tails']
+		await ctx.send(f'{random.choice(responses)}')
 
-
+	
 # @commands.command()
 # async def rand(ctx, min, max):  # not functioning yet
 #    await ctx.send(f'The number I have selected is {random.randint(min, max)}')
 
-    @commands.command(aliases=['shuffle'])
-    async def card(self, ctx):
-        value = ['Ace',
+
+	@commands.command(aliases=['shuffle'])
+	async def card(self, ctx):
+		value = ['Ace',
                  '2',
                  '3',
                  '4',
@@ -45,28 +46,19 @@ class Randomizer(commands.Cog):
                  'Jack',
                  'Queen',
                  'King']
-        suit = ['Clubs',
+		suit = ['Clubs',
                 'Hearts',
                 'Spades',
                 'Diamonds']
-        number = ['A',
-                  '2',
-                  '3',
-                  '4',
-                  '5',
-                  '6',
-                  '7',
-                  '8',
-                  '9',
-                  '10',
-                  'J',
-                  'Q',
-                  'K']
-        symbol = [':clubs:',
-                  ':heart:',
-                  ':spades:',
-                  ':diamonds:']
-        await ctx.send(f'I drew the {random.choice(value)} of {random.choice(suit)}.')
+		await ctx.send(f'I drew the {random.choice(value)} of {random.choice(suit)}.')
+
+	# error msg starts here
+	@ask.error
+	async def on_command_error(self, ctx, error):
+		if isinstance(error, commands.MissingRequiredArgument):
+			await ctx.send('What\'s the question?')
+		else:
+			raise error
 
 
 def setup(client):
